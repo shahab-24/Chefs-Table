e0619c48d46e9e14206c0e74c75eff0baa8372c4
Shahab Uddin <shahab.82946@gmail.com>
2024-03-16 21:40:21 +0600

    created sidebar div

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,53 @@ import MenuList from './assets/Components/MenuList'
 
 function App() {
   const [items, setItems] = useState([])
+  const [cooks, setCooks] = useState([])
   
 
   useEffect(() => {
     fetch('recipe.json')
     .then(res => res.json())
     .then(data => setItems(data))
-  },[])
+  },[]);
+
+  const handleItem = (p) => {
+    const isExist = cooks.find((item => item.id == item.id))
+    if(!isExist){
+      setCooks([...cooks, p])
+    } 
+    else {
+      alert('already selected')
+
+    } 
+
+    
+  }
+  console.log(cooks);
   return (
     <>
      <Navbar></Navbar>
       <Banner></Banner>
       <Recipe></Recipe>
      
-      <div className='card-container flex gap-4 shadow-sm'>
-        <div className='menu-items flex gap-2'>
-          <div>
+      <div className='card-container mx-auto grid grid-cols-2 gap-4'>
+        <div className='menu-items grid gap-2'>
+          <div className='menu grid grid-cols-2 gap-2'>
             {
-              items.map((item => <MenuList key={item.id} item={item}>
-
+              items.map((item => <MenuList key={item.id} handleItem={handleItem} item={item}>
               </MenuList>))
             }
             
-            
             </div>
-            <div>
-            <h2>hello</h2>
-            </div>
-
         </div>
-        <div className='side-bar'>
-          <h1>Want to cook: <span>0</span></h1>
+
+        <div className='side-bar menu'>
+        <h1 className='text-center text-black text-2xl'>Want to cook: <span>0</span></h1>
+          <div className='flex justify-around'>
+            <p>Name</p>
+            <p>Time</p>
+            <p>Calories</p>
+
+          </div>
 
         </div>
 
--- src/assets/Components/MenuList.jsx
+++ src/assets/Components/MenuList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const MenuList = ({item}) => {
+const MenuList = ({item, handleItem}) => {
   return (
-    <div className='flex md:flex flex-col'>
+    <div className=''>
+      <div className='menu'>
       <img className='item-image' src={item.image} alt="" />
       <h2 className='text-3xl text-black font-bold'>{item.name}</h2>
       <p className='text-black text-semibold text-xl'>{item.description}</p>
@@ -17,9 +18,10 @@ const MenuList = ({item}) => {
       <div>
       {item.calories}
       </div>
-      <button className="btn w-60 btn-primary px-4 m-4 text-black font-semibold rounded-full
+      <button onClick={(e) => handleItem(item)} className="btn w-60 btn-primary px-4 m-4 text-black font-semibold rounded-full
 
 bg-green-500">Want to Cook</button>
+    </div>
     </div>
   );
 };
